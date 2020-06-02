a595be0bba3f96a716fc8c4ee0ba6a467359d37f
Weronika Stankiewicz <weronika.stankiewicz94@gmail.com>
2020-06-02 21:55:08 +0200

    first commit - just for a try

---
+++ src/layouts/about/About.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export const About = () => {
+  return (
+    <>
+      <section className="aboutSection">
+        <p>
+          Hello! My name is Weronika. <br /> I am almost a junior front-end developer. <br />
+            Im trying to get a job at <strong>UNIT9.</strong>
+        </p>
+      </section>
+    </>
+  );
+};
+
+export default About;
---
+++ src/layouts/navigation/Navigation.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const Navigation = () => {
+  return (
+    <div>
+      hello from nav
+    </div>
+  )
+}
+export default Navigation
